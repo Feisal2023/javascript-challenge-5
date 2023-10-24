8a6e243c01a826a5b9ee382004a077be26735278
Feisal2023 <feisaladan2022@gmail.com>
2023-10-24 17:56:02 +0300

    challenge 5 completed

---
+++ script.js
@@ -0,0 +1,18 @@
+const btn = document.querySelector(".btn");
+const result = document.querySelector(".result");
+
+btn.addEventListener("click", palindrome);
+
+function palindrome() {
+  const word = document.querySelector(".input-text").value;
+  let len = word.length;
+  const start = word.substring(0, Math.floor(len / 2)).toLowerCase();
+  const end = word.substring(len - Math.floor(len / 2)).toLowerCase();
+  // let flip = end.split("").reverse().join("");
+  let flip = [...end].reserve().join("");
+  if (start == flip) {
+    result.innerHTML = `${word.toUpperCase()} is a palindrome`;
+  } else {
+    result.innerHTML = `${word.toUpperCase()} is not a palindrome`;
+  }
+}

041fcfea2b559dcb73c99132cc6fe0e52c977364
Feisal2023 <feisaladan2022@gmail.com>
2023-10-24 18:04:16 +0300

    challenge 5 completed

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function palindrome() {
   const start = word.substring(0, Math.floor(len / 2)).toLowerCase();
   const end = word.substring(len - Math.floor(len / 2)).toLowerCase();
   // let flip = end.split("").reverse().join("");
-  let flip = [...end].reserve().join("");
+  let flip = [...end].reverse().join("");
   if (start == flip) {
     result.innerHTML = `${word.toUpperCase()} is a palindrome`;
   } else {
